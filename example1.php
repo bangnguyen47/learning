f90dcbf438da8b13f0e46f5a255e1426a2a1cd0b
bangnd <bangnd@qsoft.com.vn>
2014-10-28 18:39:15 +0700

    first example

--- example1.php
+++ example1.php
@@ -1,2 +1,41 @@
 <?php
-echo 'test';
+
+/**
+ * Issue #1
+ * Please turn true, false and null of following code into uppercase
+ * Ref: https://github.com/bangnguyen47/learning/issues/1
+ */
+
+if (!defined('T_ML_COMMENT')) {
+	define('T_ML_COMMENT', T_COMMENT);
+} else {
+	define('T_DOC_COMMENT', T_ML_COMMENT);
+}
+
+$source = file_get_contents('example1.php');
+$tokens = token_get_all($source);
+echo '<pre />';
+var_dump($tokens);die;
+
+foreach ($tokens as $token) {
+	if (is_string($token)) {
+		// simple 1-character token
+		echo $token;
+	} else {
+		// token array
+		list($id, $text) = $token;
+
+		switch ($id) {
+			case T_COMMENT:
+			case T_ML_COMMENT: // we've defined this
+			case T_DOC_COMMENT: // and this
+				// no action on comments
+				break;
+
+			default:
+				// anything else -> output "as is"
+				echo $text;
+				break;
+		}
+	}
+}
