7c3ca3e61992d576ead0c7627920f95ab54cf40d
Bang Nguyen <bang.nguyen47@gmail.com>
2015-01-05 00:06:06 +0700

    update1

--- example2.php
+++ example2.php
@@ -6,7 +6,6 @@ $tokens = token_get_all ( $source );
 // echo '<pre />';
 // var_dump($tokens);die;
 
-
 $data =array();
 foreach($tokens as $token ) {
 	if(is_string($token)) {
@@ -26,22 +25,14 @@ $open_flag = 0;
 $closed_flag = 0;
 foreach ($data as $val)
 {
-	
 	if($val == '{')
 	{
-		$open_flag ++;
 		$tree[] = array($val);
+	} else if ($val == '}') {
+		$tree[$open_flag][] = $val;
+		$open_flag ++;
 	}
 	
-	if ($val == '}')
-	{
-		$closed_flag ++;
-		if ($open_flag > 0)
-		{
-			for ($i=0;$i<$open_flag;$i++)
-				$tree[] = '}'; 
-		}
-	}
 }
 echo '<pre />';
 var_dump($tree);
